664026ae88c6e3fa23ae6dfa6f43a07223964f07
boraataseven <bora@ataseven.com>
2021-11-12 01:38:30 +0300

    back provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,8 +23,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-        resolve(\Illuminate\Routing\UrlGenerator::class)->forceScheme('https');
+     
         parent::boot();
     }
 
