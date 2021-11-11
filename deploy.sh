bc833475ee9d74cd88605e863cef04cf2d0ee22b
boraataseven <bora@ataseven.com>
2021-11-12 00:40:35 +0300

    clear deploy

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,9 @@
-if [ $(git status --porcelain | wc -l) -eq "0" ]; then
-#!/bin/sh
-# activate maintenance mode
 php artisan down
-# update source code
-git pull
-# update PHP dependencies
-composer install --no-interaction --no-dev --prefer-dist
-# --no-interaction Do not ask any interactive question
-# --no-dev  Disables installation of require-dev packages.
-# --prefer-dist  Forces installation from package dist even for dev versions.
-# update database
-php artisan migrate --force
-# --force  Required to run when in production.
-# stop maintenance mode
-php artisan up
-else
-  # No changes
-fi
 
+git pull
+
+composer install --no-interaction --no-dev --prefer-dist
+
+php artisan migrate --force
+
+php artisan up
