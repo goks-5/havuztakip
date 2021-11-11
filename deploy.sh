66e50aadeed0c3eef577c0881284e98969ed6f57
boraataseven <bora@ataseven.com>
2021-11-12 00:44:50 +0300

    add some command to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,12 @@ composer install --no-interaction --no-dev --prefer-dist
 
 php artisan migrate --force
 
+php artisan cache:clear
+
+php artisan route:cache
+
+php artisan config:cache
+
+php artisan view:cache
+
 php artisan up
