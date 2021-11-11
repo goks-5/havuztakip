28f7687934e0fc24658d0dc4f8010b3c9dd50e73
boraataseven <bora@ataseven.com>
2021-11-12 01:26:42 +0300

    test fix

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,7 @@
 |
 */
 URL::forceScheme('https');
-Route::group(['prefix' => 'admin'], function () {
-    return Redirect::to('/', 301);
-});
+
 Route::post('/autocomplate', 'Ajax@AutoComplate');
 Route::get('/devicelist', 'Ajax@DeviceList')->name('devicelist');
 Route::get('/deviceonline', 'Ajax@DeviceOnline')->name('deviceonline');
@@ -44,4 +42,7 @@ Route::group(['prefix' => ''], function () {
     Route::get('/cihazlar/veriler/{id}', ['uses' => 'Devices@DeviceDatas', 'as' => 'cihazveriler']);
     Voyager::routes();
     // Route::get('/ekran', ['uses' => 'Dashboards@index',   'as' => 'voyager.dashboard']);
+});
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
