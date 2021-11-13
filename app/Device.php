d224183bda333ccaac181c106b49b72778bb7339
boraataseven <bora@ataseven.com>
2021-11-14 01:17:25 +0300

    tags add fillable

--- app/Device.php
+++ app/Device.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Log;
 class Device extends Model
 {
 
-    protected $fillable = ['token','last_data','tags_last_change','last_at'];
+    protected $fillable = ['token','last_data','tags','tags_last_change','last_at'];
     public static function hourly()
     {
         //  DB::enableQueryLog();
