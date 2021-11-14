ff95797167d22e7a28ca507e764db83144fa7ee0
boraataseven <bora@ataseven.com>
2021-11-14 22:56:01 +0300

    delete company user when company delete

--- app/Http/Controllers/Companies.php
+++ app/Http/Controllers/Companies.php
@@ -8,7 +8,10 @@ use App\User;
 
 use App\Company;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\VoyagerBaseController;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Events\BreadDataDeleted;
 
 class Companies extends VoyagerBaseController
 {
@@ -33,4 +36,53 @@ class Companies extends VoyagerBaseController
       return redirect()->route('voyager.login');
     }
   }
+
+  public function destroy(Request $request, $id)
+  {
+      $slug = $this->getSlug($request);
+
+      $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+      // Check permission
+      $this->authorize('delete', app($dataType->model_name));
+
+      // Init array of IDs
+      $ids = [];
+      if (empty($id)) {
+          // Bulk delete, get IDs from POST
+          $ids = explode(',', $request->ids);
+      } else {
+          // Single item delete, get ID from URL
+          $ids[] = $id;
+      }
+      foreach ($ids as $id) {
+          $data = call_user_func([$dataType->model_name, 'findOrFail'], $id);
+
+          $model = app($dataType->model_name);
+          if (!($model && in_array(SoftDeletes::class, class_uses($model)))) {
+              $this->cleanup($dataType, $data);
+          }
+      }
+
+      $displayName = count($ids) > 1 ? $dataType->getTranslatedAttribute('display_name_plural') : $dataType->getTranslatedAttribute('display_name_singular');
+
+      $res = $data->destroy($ids);
+      $data = $res
+          ? [
+              'message'    => __('voyager::generic.successfully_deleted') . " {$displayName}",
+              'alert-type' => 'success',
+          ]
+          : [
+              'message'    => __('voyager::generic.error_deleting') . " {$displayName}",
+              'alert-type' => 'error',
+          ];
+
+      if ($res) {
+          event(new BreadDataDeleted($dataType, $data));
+      }
+      DB::table("company_users")->whereIn('company_id' ,$ids)->delete();
+      return redirect()->route("voyager.{$dataType->slug}.index")->with($data);
+  }
+
+
 }
