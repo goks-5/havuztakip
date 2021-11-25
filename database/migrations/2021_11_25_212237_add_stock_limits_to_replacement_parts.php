6daaaed1bb399438dc7eb1fff3912e4ed56d7a06
boraataseven <bora@ataseven.com>
2021-11-25 21:59:06 +0300

    add data row to migrations

--- database/migrations/2021_11_25_212237_add_stock_limits_to_replacement_parts.php
+++ database/migrations/2021_11_25_212237_add_stock_limits_to_replacement_parts.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddStockLimitsToReplacementParts extends Migration
@@ -17,6 +18,42 @@ class AddStockLimitsToReplacementParts extends Migration
             $table->integer('lower_limit')->default(0)->after('unit');
             $table->integer('upper_limit')->default(1000)->after('unit');
         });
+
+        $type = DB::table('data_types')->where("name" , 'replacement_parts')->first();
+
+        DB::table('data_rows')->insert(
+            array(
+                'data_type_id' => $type->id ,
+                'field' => 'lower_limit',
+                'type' => 'number',
+                'display_name' => 'Alt Limit',
+                'required' => '0',
+                'browse' => '1',
+                'read' => '1',
+                'edit' => '1',
+                'add' => '1',
+                'delete' => '1',
+                'details' => '{"default":"0"}',
+                'order' => '8',
+            )
+        );
+        DB::table('data_rows')->insert(
+            array(
+                'data_type_id' => $type->id ,
+                'field' => 'upper_limit',
+                'type' => 'number',
+                'display_name' => 'Üst Limit',
+                'required' => '0',
+                'browse' => '1',
+                'read' => '1',
+                'edit' => '1',
+                'add' => '1',
+                'delete' => '1',
+                'details' => '{"default":"1000"}',
+                'order' => '8',
+            )
+        );
+        
     }
 
     /**
