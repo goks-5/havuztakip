44651e699ad932178da4a34d5b844c981e9acce5
boraataseven <bora@ataseven.com>
2021-12-11 00:42:55 +0300

    consolo log

--- resources/views/vendor/voyager/custom-reports/edit-add.blade.php
+++ resources/views/vendor/voyager/custom-reports/edit-add.blade.php
@@ -190,6 +190,9 @@ function addtomodal(cell){
   $('#targetCell').val(cell);
   $('#cellType').val($('#type_'+cell).val());
   $('#cellText').val($('#text_'+cell).val());
+  console.log($('#text_'+cell);
+
+  $('#device').trigger("change");
   $('#deviceTags').val($('#value_'+cell).val());
 }
 
