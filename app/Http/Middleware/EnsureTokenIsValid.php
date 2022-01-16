5b24d50d52f2ee402bc1f9b26c114d540cdcf67c
boraataseven <bora@ataseven.com>
2022-01-16 13:55:53 +0300

    token error logs

--- app/Http/Middleware/EnsureTokenIsValid.php
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Device;
 use App\UndefineDevice;
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class EnsureTokenIsValid
 {
@@ -21,6 +22,7 @@ class EnsureTokenIsValid
         $parameters = $request->all();
 
         if (!isset($parameters['mac']) || !isset($parameters['device_id']) || !isset($parameters['data'])) {
+            Log::error("missing parameters",$parameters);
             return response()->json(['status' => 'error', 'message' => 'missing parameters', 'timestamp' => time() , 'date' => date('Y-m-d H:i:s') ], 400);
         }
         $device = Device::where(['mac' => $parameters['mac'], 'device_id' => $parameters['device_id']])->first();
@@ -28,11 +30,13 @@ class EnsureTokenIsValid
             if ($this->validateData($parameters, $device)) {
                 return $next($request);
             } else {
+                Log::error("data not validate",$parameters);
                 return response()->json(['status' => 'error', 'message' => 'data not validate', 'timestamp' => time(), 'date' => date('Y-m-d H:i:s') ], 403);
             }
         } else {
             $uDevice = UndefineDevice::firstOrNew(['mac' => $parameters['mac'], 'device_id' => $parameters['device_id']]);
             $uDevice->save();
+            Log::warning("device not found",$parameters);
             return response()->json(['status' => 'warning', 'message' => 'device not found', 'timestamp' => time(), 'date' => date('Y-m-d H:i:s') ], 404);
         }
     }
