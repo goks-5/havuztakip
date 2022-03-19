3fc3a6d177d68890221c9c3dc1abc01adbc2c655
boraataseven <bora@ataseven.com>
2022-03-19 13:18:00 +0300

    buton

--- resources/views/dashboard/tools/device_chart.blade.php
+++ resources/views/dashboard/tools/device_chart.blade.php
@@ -8,5 +8,7 @@
   <div class="col-xs-12 text-center">{{$settings['title']}} </div>
   @endif
   <div class="col-xs-12" id="tool_{{$tool->id}}" data-type="{{$setting['type'] ?? 'line'}}" style="height: calc(100% - 6px); overflow: hidden;"></div>
-  <button onclick="printDiv('tool_{{$tool->id}}')"><i class="voyager-print"></i></button>
+  <a href="#" class="btn btn-primary btn-sm edit"  onclick="printDiv('tool_{{$tool->id}}')">
+    <i class="voyager-print"></i> Yazdir
+    </a>
 </div>
