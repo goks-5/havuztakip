bfb63d43c4d0d792d6648c4441e899842293975e
boraataseven <bora@ataseven.com>
2022-07-23 10:53:17 +0300

    disable log clear

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,6 @@ php artisan down
 
 df -lh / 
 
-echo "log dosyaları siliniyor...."
-
-find /var/log/ -type f >logdosyalari.txt
-while read line
-do
-    NAME=echo "$line" | cut -d'.' -f1
-    EXTENSION=echo "$line" | cut -d'.' -f2
-    rm -rf $NAME.gz
-    :> "$line";
-done <logdosyalari.txt
-rm -rf logdosyalari.txt
-rm -rf /var/log/-2
-rm -rf /var/log/.2
-
-echo "log dosyaları silindi"
-
-df -lh / 
-
 git pull
 
 composer install --no-interaction --no-dev --prefer-dist
