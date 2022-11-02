608e5e921af4643a4652e0ca29050d8be7d7b97f
boraataseven <bora@ataseven.com>
2022-11-03 01:49:58 +0300

    server info add system static

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -30,7 +30,29 @@ class Controller extends VoyagerBaseController
     $data['commit_date'] = trim(exec('git log -n1 --pretty=%ci HEAD'));
 
     $data['ip'] = request()->server('SERVER_ADDR');
+    
+    $load = sys_getloadavg();
 
+    $data['cpu_load_1_minute'] = $load[0] * 100  . "%" ;
+    
+    $data['cpu_load_5_minutes'] = $load[1] * 100  . "%" ;
+    
+    $data['memory_usage'] = self::get_server_memory_usage() ;
+    
     return response()->json($data);
   }
+
+  public static function get_server_memory_usage(){
+
+    $free = shell_exec('free');
+    $free = (string)trim($free);
+    $free_arr = explode("\n", $free);
+    $mem = explode(" ", $free_arr[1]);
+    $mem = array_filter($mem);
+    $mem = array_merge($mem);
+    $memory_usage = $mem[2]/$mem[1]*100;
+
+    return $memory_usage;
+  }
+
 }
