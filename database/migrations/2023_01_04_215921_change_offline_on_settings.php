4d485534b08ec1b0e35cb797166081a808bd99bc
boraataseven <bora@ataseven.com>
2023-01-04 22:48:08 +0300

    chance device ofline time

---
+++ database/migrations/2023_01_04_215921_change_offline_on_settings.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use TCG\Voyager\Models\Setting;
+
+class ChangeTagChangeOnSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $setting = Setting::where('key', 'device.ofline')->first();
+        if ($setting) {
+            $setting->details = '{"default" : "5",
+                "options" : {
+                "5" : "5 dk" ,
+                "10":"10 dk" ,
+                "15":"15 dk",
+                "30":"30 dk",
+                "60":"1 Saat",
+                "120":"2 Saat",
+                "180":"3 Saat",
+                "360":"6 Saat",
+                "570":"9 Saat",
+                "720":"12 Saat",
+                "900":"15 Saat",
+                "1080":"18 Saat",
+                "1440":"1 Gün"}
+            }';$setting->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $setting = Setting::where('key', 'device.ofline')->first();
+        if ($setting) {
+            $setting->details = '{"default" : "5",
+                "options" : {
+                "5" : "5 dk" ,
+                "10":"10 dk" ,
+                "15":"15 dk",
+                "30":"30 dk",
+                "60":"1 Saat",
+                "120":"2 Saat",
+                "180":"3 Saat"}
+            }';
+            $setting->save();
+        }
+    }
+}
