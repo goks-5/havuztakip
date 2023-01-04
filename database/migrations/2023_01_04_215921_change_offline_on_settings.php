7bbfb5d253d35f55bf91869111a720a9345115d9
boraataseven <bora@ataseven.com>
2023-01-04 22:51:45 +0300

    fix migrationclass  Name

--- database/migrations/2023_01_04_215921_change_offline_on_settings.php
+++ database/migrations/2023_01_04_215921_change_offline_on_settings.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use TCG\Voyager\Models\Setting;
 
-class ChangeTagChangeOnSettings extends Migration
+class ChangeOfflineOnSettings extends Migration
 {
     /**
      * Run the migrations.
