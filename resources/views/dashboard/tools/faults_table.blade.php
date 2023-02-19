1b7830bbb1c9e7533b681de09cc76f86fe3b183e
boraataseven <bora@ataseven.com>
2023-02-19 18:37:25 +0300

    test

--- resources/views/dashboard/tools/faults_table.blade.php
+++ resources/views/dashboard/tools/faults_table.blade.php
@@ -1,7 +1,7 @@
 <div class="tool_data row">
   @include('dashboard.tools.toolSettings',['tool'=>$tool])
 
-@dump($settings['status'][0])
+@dump($settings['status']['0'])
 
 
     <div id="tool_{{$tool->id}}"></div>
