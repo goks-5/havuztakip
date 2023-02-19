80ee28f5d9b76811fcac5160b6851f89c9cdac58
boraataseven <bora@ataseven.com>
2023-02-19 18:34:17 +0300

    test

--- resources/views/dashboard/tools/faults_table.blade.php
+++ resources/views/dashboard/tools/faults_table.blade.php
@@ -1,7 +1,11 @@
 <div class="tool_data row">
   @include('dashboard.tools.toolSettings',['tool'=>$tool])
 
+@dump($settings)
+
+
     <div id="tool_{{$tool->id}}"></div>
+
     @can('browse',app('App\Fault'))
 
       @endcan
