962f138cc1dd191e54bd4183c2e674ceb6e26683
boraataseven <bora@ataseven.com>
2023-02-19 18:25:12 +0300

    test

--- resources/views/ajax/dashboard/type/faults_table.blade.php
+++ resources/views/ajax/dashboard/type/faults_table.blade.php
@@ -4,27 +4,27 @@
 
 <h5>Durumlar</h5>
  <div class="col-xs-3">
-      <label class="control-label">Bekliyor</label>
+      <label class="control-label">Bekliyor {{ $setting['status']['0'] ?? '0' }}</label> 
       <input type="checkbox" name="setting[status][0]" class="form-check-input" style="display:block" value="Bekliyor |0|"
           id='sound' {{ $setting['status']['0'] ?? '0' == 'Bekliyor |0|' ? 'checked' : '' }} />
   </div>
   <div class="col-xs-3">
-    <label class="control-label">Bakıma Başlandı</label>
+    <label class="control-label">Bakıma Başlandı {{ $setting['status']['1'] ?? '0' }}</label>
     <input type="checkbox" name="setting[status][1]" class="form-check-input" style="display:block" value="Bakıma Başlandı |0|"
         id='sound' {{ $setting['status']['1'] ?? '0'  == 'Bakıma Başlandı |0|' ? 'checked' : '' }} />
 </div>
 <div class="col-xs-3">
-  <label class="control-label">Firma Yönlendirildi</label>
+  <label class="control-label">Firma Yönlendirildi {{ $setting['status']['2'] ?? '0' }}</label>
   <input type="checkbox" name="setting[status][2]" class="form-check-input" style="display:block" value="Firma Yönlendirildi |2|"
       id='sound' {{ $setting['status']['2'] ?? '0'  == 'Firma Yönlendirildi |2|' ? 'checked' : '' }} />
 </div>
 <div class="col-xs-3">
-  <label class="control-label">Malzeme Bekliyor</label>
+  <label class="control-label">Malzeme Bekliyor {{ $setting['status']['3'] ?? '0' }}</label>
   <input type="checkbox" name="setting[status][3]" class="form-check-input" style="display:block" value="Malzeme Bekliyor |2|"
       id='sound' {{ $setting['status']['3'] ?? '0'  == 'Malzeme Bekliyor |2|' ? 'checked' : '' }} />
 </div>
 <div class="col-xs-3">
-  <label class="control-label">Tamamlandı</label>
+  <label class="control-label">Tamamlandı {{ $setting['status']['4'] ?? '0' }}</label>
   <input type="checkbox" name="setting[status][4]" class="form-check-input" style="display:block" value="Onay |1|"
       id='sound' {{ $setting['status']['4'] ?? '0'  == 'Onay |1|' ? 'checked' : '' }} />
 </div>
