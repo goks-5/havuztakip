5bd17239677aa6c064d0e7ab200bc2e57ed4b98e
boraataseven <bora@ataseven.com>
2023-02-19 18:44:49 +0300

    beuty form

--- resources/views/ajax/dashboard/type/faults_table.blade.php
+++ resources/views/ajax/dashboard/type/faults_table.blade.php
@@ -2,40 +2,48 @@
   @include('ajax.dashboard.type.element.main', ['slug' => 'faults_table', 'ek' => 1])
   <br>
 
-<h5>Durumlar</h5>
- <div class="col-xs-3">
-      <label class="control-label">Bekliyor</label> 
-      <input type="checkbox" name="setting[status][0]" class="form-check-input" style="display:block" value="Bekliyor |0|"
-          id='setting[status][0]' {{ ($settings['status']['0'] ?? '0') == 'Bekliyor |0|' ? 'checked' : '' }} />
+  <h5>Durumlar</h5>
+  <div>
+      <div class="col-xs-3">
+          <label class="control-label">Bekliyor</label>
+          <input type="checkbox" name="setting[status][0]" class="form-check-input" style="display:block"
+              value="Bekliyor |0|" id='setting[status][0]'
+              {{ ($settings['status']['0'] ?? '0') == 'Bekliyor |0|' ? 'checked' : '' }} />
+      </div>
+      <div class="col-xs-3">
+          <label class="control-label">Bakıma Başlandı</label>
+          <input type="checkbox" name="setting[status][1]" class="form-check-input" style="display:block"
+              value="Bakıma Başlandı |0|" id='setting[status][1]'
+              {{ ($settings['status']['1'] ?? '0') == 'Bakıma Başlandı |0|' ? 'checked' : '' }} />
+      </div>
+      <div class="col-xs-3">
+          <label class="control-label">Firma Yönlendirildi</label>
+          <input type="checkbox" name="setting[status][2]" class="form-check-input" style="display:block"
+              value="Firma Yönlendirildi |2|" id='setting[status][2]'
+              {{ ($settings['status']['2'] ?? '0') == 'Firma Yönlendirildi |2|' ? 'checked' : '' }} />
+      </div>
+      <div class="col-xs-3">
+          <label class="control-label">Malzeme Bekliyor</label>
+          <input type="checkbox" name="setting[status][3]" class="form-check-input" style="display:block"
+              value="Malzeme Bekliyor |2|" id='setting[status][3]'
+              {{ ($settings['status']['3'] ?? '0') == 'Malzeme Bekliyor |2|' ? 'checked' : '' }} />
+      </div>
+      <div class="col-xs-3">
+          <label class="control-label">Tamamlandı</label>
+          <input type="checkbox" name="setting[status][4]" class="form-check-input" style="display:block"
+              value="Onay |1|" id='setting[status][4]'
+              {{ ($settings['status']['4'] ?? '0') == 'Onay |1|' ? 'checked' : '' }} />
+      </div>
   </div>
-  <div class="col-xs-3">
-    <label class="control-label">Bakıma Başlandı</label>
-    <input type="checkbox" name="setting[status][1]" class="form-check-input" style="display:block" value="Bakıma Başlandı |0|"
-        id='setting[status][1]' {{ ($settings['status']['1'] ?? '0')  == 'Bakıma Başlandı |0|' ? 'checked' : '' }} />
-</div>
-<div class="col-xs-3">
-  <label class="control-label">Firma Yönlendirildi</label>
-  <input type="checkbox" name="setting[status][2]" class="form-check-input" style="display:block" value="Firma Yönlendirildi |2|"
-      id='setting[status][2]' {{ ($settings['status']['2'] ?? '0')  == 'Firma Yönlendirildi |2|' ? 'checked' : '' }} />
-</div>
-<div class="col-xs-3">
-  <label class="control-label">Malzeme Bekliyor</label>
-  <input type="checkbox" name="setting[status][3]" class="form-check-input" style="display:block" value="Malzeme Bekliyor |2|"
-      id='setting[status][3]' {{ ($settings['status']['3'] ?? '0')  == 'Malzeme Bekliyor |2|' ? 'checked' : '' }} />
-</div>
-<div class="col-xs-3">
-  <label class="control-label">Tamamlandı</label>
-  <input type="checkbox" name="setting[status][4]" class="form-check-input" style="display:block" value="Onay |1|"
-      id='setting[status][4]' {{ ($settings['status']['4'] ?? '0')  == 'Onay |1|' ? 'checked' : '' }} />
-</div>
-<br>
-<div class="col-xs-3">
-  <label class="control-label">Veri Gösterme Limiti</label>
-<input type="number" name="setting[limit]" class="form-control" value="{{$settings['limit'] ?? '10'}}" />
-</div>
+  <br>
 
-<br>
-<br>
-<br>
-<br>
-<br>
+  <div class="col-xs-3">
+      <label class="control-label">Veri Gösterme Limiti</label>
+      <input type="number" name="setting[limit]" class="form-control" value="{{ $settings['limit'] ?? '10' }}" />
+  </div>
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
