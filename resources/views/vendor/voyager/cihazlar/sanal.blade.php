ebe2b33386fce2c4027ed601c910b6e35f01688e
Bora Ataseven <bora@ataseven.com>
2023-03-19 00:58:33 +0300

    test

--- resources/views/vendor/voyager/cihazlar/sanal.blade.php
+++ resources/views/vendor/voyager/cihazlar/sanal.blade.php
@@ -226,7 +226,6 @@
                 var filter = $(this).val();
                 console.log(filter);
                 console.log($(this).nextAll('.islem:first'));
-                console.log($(this).nextAll('.islem:first').children(".deviceTagOption"));
                 $(this).nextAll('.islem:first').children(".deviceTagOption").each(function() {
                     var match = $(this).text().search(new RegExp(filter, "i"));
                     console.log(match);
