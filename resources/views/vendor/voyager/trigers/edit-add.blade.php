ac7f587b77ccb436d1b8c186dcf65edc59a9067b
boraataseven <bora@ataseven.com>
2023-05-13 16:24:37 +0300

    last view fix

--- resources/views/vendor/voyager/trigers/edit-add.blade.php
+++ resources/views/vendor/voyager/trigers/edit-add.blade.php
@@ -81,15 +81,6 @@
                             </div>
 
 
-                            <div class="form-group  col-md-12 ">
-                                <label class="control-label" for="name">Yetkili Kişiler</label>
-                                <select class="form-control select2" name="users" multiple>
-                                    <option value>None</option>
-                                    @foreach ($users as $user)
-                                        <option value="{{ $user->email }}">{{ $user->email }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
 
 
 
@@ -146,6 +137,15 @@
                                 @endif
                             @endforeach
 
+                            <div class="form-group  col-md-12 ">
+                                <label class="control-label" for="name">Gönderilecek Mailler</label>
+                                <select class="form-control select2" name="users" multiple>
+                                    <option value>None</option>
+                                    @foreach ($users as $user)
+                                        <option value="{{ $user->email }}">{{ $user->email }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                         </div><!-- panel-body -->
 
                         <div class="panel-footer">
