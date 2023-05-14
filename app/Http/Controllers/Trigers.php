a073bdfb621495c1dedd344dc8352ae391989d8f
boraataseven <bora@ataseven.com>
2023-05-15 00:49:20 +0300

    f

--- app/Http/Controllers/Trigers.php
+++ app/Http/Controllers/Trigers.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\VoyagerBaseController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class Trigers extends VoyagerBaseController
 {
+
+    public function store(Request $request)
+    {
+        dd($request);
+        return parent::store($request);
+    }
+
+    public function update(Request $request, $id)
+    {
+        return parent::update($request, $id);
+    }
+
     public function insertUpdateData($request, $slug, $rows, $data)
     {
         dd($request);
-        return parent::insertUpdateData($request, $slug, $rows, $data) ;
+        return parent::insertUpdateData($request, $slug, $rows, $data);
     }
 }
