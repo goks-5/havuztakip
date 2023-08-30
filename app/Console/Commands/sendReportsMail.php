f48c9e37b3a106412e8ac7db006c6cf1b3cb0406
boraataseven <bora@ataseven.com>
2023-08-30 13:09:03 +0300

    fix

--- app/Console/Commands/sendReportsMail.php
+++ app/Console/Commands/sendReportsMail.php
@@ -66,7 +66,7 @@ class sendReportsMail extends Command
 
 
     public function sendMail($mails,$data,$title){
-        $path = public_path('exports/' . Str::slug($title) );
+        $path = public_path('exports/' . Str::slug($title) . '.xlsx' );
         Excel::store(new ReportExport(array_values($data)), $path);
         $emailContent = "Enerji Yönetim den otomatik oluşturulan raporu ekden indirebilirsiniz.";
         foreach($mails as $mail){
