d8b44ce80dbf43229af3004190703af107397f2d
Bora Ataseven <bora@ataseven.com>
2023-12-21 15:57:04 +0300

    arızaları 13 ayla limitle

--- app/Fault.php
+++ app/Fault.php
@@ -4,12 +4,30 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 
 class Fault extends Model
 {
     protected $casts = [
         'accepted_at' => 'date',
     ];
+
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        // Belirli bir tarihten eski olanları otomatik olarak filtrele
+        $this->filterOldRecords();
+    }
+
+    private function filterOldRecords()
+    {
+        $thresholdDate = Carbon::now()->subMonths(13);
+
+    
+        $this->where('created_at', '>=', $thresholdDate);
+    }
 
     public function equipment(): BelongsTo
     {
