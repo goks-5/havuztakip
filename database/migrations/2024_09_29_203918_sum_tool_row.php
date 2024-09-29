7dc8f0e6c0068d7e761ab319488def7ae0d32802
Bora Ataseven <bora@ataseven.com>
2024-09-29 21:53:10 +0300

    fix migration

--- database/migrations/2024_09_29_203918_sum_tool_row.php
+++ database/migrations/2024_09_29_203918_sum_tool_row.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class NewToolRow extends Migration
+class SumToolRow extends Migration
 {
     /**
      * Run the migrations.
