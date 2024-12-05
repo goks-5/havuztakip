7961bc057135ba12d11d3296162260c84c05279d
Bora Ataseven <bora@ataseven.com>
2024-12-05 21:38:42 +0300

    fix classname

--- database/migrations/2024_12_05_213400_addresource_and_fileds_to_data_rows.php
+++ database/migrations/2024_12_05_213400_addresource_and_fileds_to_data_rows.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class UpdateOrInsertDataRows extends Migration
+class AddresourceAndFiledsToDataRows extends Migration
 {
     /**
      * Run the migrations.
