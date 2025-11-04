da3f378fa07edb2d480c77f6be59db9a5e2eb2e9
goks-5 <gookceturun@gmail.com>
2025-11-04 10:35:27 +0300

    company-avatar-problem-fix

--- resources/views/vendor/voyager/firmalar/switch.blade.php
+++ resources/views/vendor/voyager/firmalar/switch.blade.php
@@ -23,38 +23,36 @@
                       <thead>
                       <tr>
 
-                      <th>
-Firma
-
-                      </th>
-<th></th>
+                      <th>Firma</th>
+                        <th></th>
 
                       <th class="actions text-right">İşlemler</th>
                       </tr>
                       </thead>
                       <tbody>
 
-                      @foreach ($companies as $company)
-                        <tr>
-                         <td>
-<img src="@if( !filter_var($company->avatar, FILTER_VALIDATE_URL)){{ Voyager::image( $company->avatar ) }}@else{{ $company->avatar }}@endif"/>
-                        </td>
-                        <td>
-{{$company->name}}"
-                       </td>
-                        <td class="no-sort no-click" id="bread-actions">
-
-                         <a href="{{route('switch_company',['id'=>$company->id])}}" title="Geçiş Yap" class="btn btn-sm btn-warning pull-right view">
-                        <i class="voyager-eye"></i> <span class="hidden-xs hidden-sm">Geçiş Yap</span>
-                        </a>
-                        </td>    </tr>
-                      @endforeach
-
+                     @foreach ($companies as $company)
+                        @if ($company)
+                            <tr>
+                            <td>
+                                @if (!empty($company->avatar))
+                                <img src="@if( !filter_var($company->avatar, FILTER_VALIDATE_URL)){{ Voyager::image($company->avatar) }}@else{{ $company->avatar }}@endif" width="50" height="50" />
+                                @else
+                                <img src="{{ asset('storage/users/default.png') }}" width="50" height="50" />
+                                @endif
+                            </td>
+                            <td>{{ $company->name ?? 'Firma Adı Yok' }}</td>
+                            <td class="no-sort no-click" id="bread-actions">
+                                <a href="{{ route('switch_company', ['id' => $company->id]) }}" title="Geçiş Yap" class="btn btn-sm btn-warning pull-right view">
+                                <i class="voyager-eye"></i> <span class="hidden-xs hidden-sm">Geçiş Yap</span>
+                                </a>
+                            </td>
+                            </tr>
+                        @endif
+                    @endforeach
 
                       </tbody>
                       </table>
-
-
 </div>
 </div>
 </div>
@@ -69,6 +67,4 @@ Firma
 
                       @section('javascript')
                           <!-- DataTables -->
-
-
                       @stop
